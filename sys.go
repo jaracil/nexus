55740c995296b01a569d5d8dada8172ab6ba301e
Jaime <jbreva@nayarsystems.com>
2016-07-18 10:22:15 +0200

    Mask authenticator tags with local tags

--- sys.go
+++ sys.go
@@ -27,11 +27,10 @@ func (nc *NexusConn) handleSysReq(req *JsonRpcReq) {
 
 		// Auth
 		switch ei.N(req.Params).M("method").StringZ() {
-		case "token":
-			user, tags, err = nc.TokenAuth(req.Params)
-
 		default:
 			user, tags, err = nc.BasicAuth(req.Params)
+		case "token":
+			user, tags, err = nc.TokenAuth(req.Params)
 		}
 
 		if err != ErrNoError {
@@ -39,11 +38,36 @@ func (nc *NexusConn) handleSysReq(req *JsonRpcReq) {
 			return
 		}
 
-		atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&nc.user)), unsafe.Pointer(&UserData{User: user, Tags: tags}))
+		ud, err := loadUserData(user)
+		if err != ErrNoError {
+			req.Error(err, "", nil)
+			return
+		}
+
+		tags = maskTags(tags, ud.Tags)
+
+		atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&nc.user)), unsafe.Pointer(&UserData{User: ud.User, Tags: tags}))
 		nc.updateSession()
 		req.Result(ei.M{"ok": true, "user": nc.user.User})
 
 	default:
 		req.Error(ErrMethodNotFound, "", nil)
 	}
+}
+
+func maskTags(a map[string]map[string]interface{}, b map[string]map[string]interface{}) (m map[string]map[string]interface{}) {
+	m = make(map[string]map[string]interface{})
+
+	for prefix, tags := range a {
+		if bprefix, ok := b[prefix]; ok {
+			m[prefix] = make(map[string]interface{})
+			for k, v := range tags {
+				if vb, ok := bprefix[k]; ok && v == vb {
+					m[prefix][k] = v
+				}
+			}
+		}
+	}
+
+	return
 }
